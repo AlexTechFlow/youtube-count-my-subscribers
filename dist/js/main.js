fddfca311e24149dd01343d4bbb9dd7c7391f313
AlexTechFlow <designdigitalhealth@gmail.com>
2018-11-06 13:06:12 -0500

    updated js code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,51 @@
-'use strict';
+"use strict";
 
-// 1. Создаём новый объект XMLHttpRequest
-var xhr = new XMLHttpRequest();
+var HttpClient = function HttpClient() {
 
-// 2. Конфигурируем его: GET-запрос на URL 'google server with parameters added'
-xhr.open('GET', 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyA1mDALQf2z4NA6-wkj513DJxG7FwozOvQ', false);
+	this.get = function (aUrl, aCallback) {
 
-// 3. Отсылаем запрос
-xhr.send();
+		var anHttpRequest = new XMLHttpRequest();
 
-// 4. Если код ответа сервера не 200, то это ошибка
-if (xhr.status != 200) {
-  // обработать ошибку
-  console.log(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
-} else {
+		anHttpRequest.onreadystatechange = function () {
+			if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+		};
 
-  // вывести результат
-  var subsCount = JSON.parse(xhr.responseText).items[0].statistics;
-  //console.log(JSON.parse(xhr.responseText).items[0].statistics); // responseText -- текст ответа.
+		anHttpRequest.open("GET", aUrl, true);
+		anHttpRequest.send(null);
+	};
+};
 
-  document.querySelector('#count').innerText = subsCount;
-}
+var client = new HttpClient();
 
-// GET https://www.googleapis.com/youtube/v3/channels
+var showStat = function showStat() {
+	client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyA1mDALQf2z4NA6-wkj513DJxG7FwozOvQ', function (response) {
+		console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+		console.log(JSON.parse(response).items[0].channelDescription); // see docs or console log the object
+	});
+};
+
+showStat();
+
+// // 1. Создаём новый объект XMLHttpRequest
+// var xhr = new XMLHttpRequest();
+
+// // 2. Конфигурируем его: GET-запрос на URL 'google server with parameters added'
+// xhr.open('GET', 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyA1mDALQf2z4NA6-wkj513DJxG7FwozOvQ', false);
+
+// // 3. Отсылаем запрос
+// xhr.send();
+
+// // 4. Если код ответа сервера не 200, то это ошибка
+// if (xhr.status != 200) {
+//   // обработать ошибку
+//   console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+// } else {
+
+//   // вывести результат
+//   var subsCount = JSON.parse(xhr.responseText).items[0].statistics;
+//   //console.log(JSON.parse(xhr.responseText).items[0].statistics); // responseText -- текст ответа.
+
+//   document.querySelector('#count').innerText = subsCount;
+// }
+
+// // GET https://www.googleapis.com/youtube/v3/channels
