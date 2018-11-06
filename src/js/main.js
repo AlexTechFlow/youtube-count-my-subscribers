4717c022a480599a39bffd12abb7b6dd0be7bdf9
AlexTechFlow <designdigitalhealth@gmail.com>
2018-11-06 12:36:02 -0500

    Updated JS code - made asynchronous requests

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,12 @@ if (xhr.status != 200) {
   // обработать ошибку
   console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
 } else {
+	
   // вывести результат
   var subsCount = JSON.parse(xhr.responseText).items[0].statistics;
   //console.log(JSON.parse(xhr.responseText).items[0].statistics); // responseText -- текст ответа.
+
+  document.querySelector('#count').innerText = subsCount;
 }
 
 // GET https://www.googleapis.com/youtube/v3/channels
